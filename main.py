d13cc666ff80fcfefe3ffedea494afa8f45c28f5
nikhil <nikhiltelase@gmail.com>
2024-04-02 01:27:30 +0530

    update code

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+import requests as sheet
+import smtplib
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    name = request.form['fullName']
+    email = request.form['email']
+    mobile = request.form['mobile']
+    message = request.form['message']
+    # saving data into google sheet
+    data = {
+        'sheet1': {
+            'name': name,
+            'email': email,
+            'mobile': mobile,
+            'message': message,
+        }
+    }
+    sheet.post("https://api.sheety.co/06611c666efcc4e05beb6ca9c244f4fa/visitors/sheet1", json=data)
+
+    # sending mail
+    my_email = "nikhiltelase17@gmail.com"
+    password = "qyhtmfnyyebfayqz"
+    to_mail = email
+    mail_message = f"Subject: Thankyou \n\nThankyou {name} visiting my web.\nI will contact soon.\nYour message if valuble for me"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()  # seure
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=[to_mail],
+                            msg=mail_message)
+    return render_template("thankyou.html", name=name)
+
+
+@app.route('/')
+def go_back():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
